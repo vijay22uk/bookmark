ba4cd98caa2c9fb36b771ed71d76bf77de806dfc
viajy22uk <vijay22uk@gmail.com>
2016-10-03 15:46:12 +0530

    implemented custom flux

--- dispatcher.js
+++ dispatcher.js
@@ -1,2 +1,20 @@
-import {Dispatcher} from 'flux';
-export default new Dispatcher;
+// import {Dispatcher} from 'flux';
+// export default new Dispatcher;
+import guid from 'guid';
+var listeners = {};
+
+module.exports = {
+    register: function (cb) {
+        var id = guid.raw();
+        listeners[id] = cb;
+        return id;
+    },
+    dispatch: function (payload) {
+        for (var id in listeners) {
+            if (listeners.hasOwnProperty(id)) {
+                listeners(payload);
+            }
+        }
+    }
+
+}
